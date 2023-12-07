b664e0a59a618df79f46eb839dc4a1ff7249664d
xayyeem <kjafri30izback@gmail.com>
2023-12-07 11:17:43 +0530

    setting up pages and component

---
+++ vite-project/src/Components/SearchDialogBox/Searchdialogbox.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Searchdialogbox = () => {
+  return (
+    <div>Searchdialogbox</div>
+  )
+}
+
+export default Searchdialogbox
---
+++ vite-project/src/firebase/FirebaseConfig.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FirebaseConfig = () => {
+  return (
+    <div>FirebaseConfig</div>
+  )
+}
+
+export default FirebaseConfig
